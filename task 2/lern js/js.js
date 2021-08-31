ad30bce4021efc5424301856d6cf768722f0c723
Mohammed nasr ElSheikh <87213269+ElSheikh0@users.noreply.github.com>
2021-08-31 04:10:46 +0200

    Add files via upload

---
+++ task 2/lern js/js.js
@@ -0,0 +1,14 @@
+//console.table(['mo', 'r', 'l']);
+//console.group(arr, ['nnnnn']);
+console.log("This is the outer level");
+console.group("aa");
+console.log("Level 2");
+console.group();
+console.log("Level 3");
+console.warn("More of level 3");
+console.groupEnd();
+console.log("Back to level 2");
+console.groupEnd();
+console.log("Back to the outer level");
+
+console.log("mlkkk%ckl", "color: red ; font-size: 50px;font-width:bold")
